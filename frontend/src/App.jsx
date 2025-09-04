95c2c8261f3a3f2585c7f41e29b944d6eab8c193
amine <mo.amine.alt@gmail.com>
2025-09-04 16:33:55 +0100

    updating styles by adding tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast"
 
 function App() {
-
-  return <h1>Hello world</h1>
+    return (
+        <div className="App">
+            <button class="btn btn-primary">Button</button>
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/create" element={<CreatePage/>} />
+                <Route path="/note/:id" element={<NoteDetailPage/>} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
